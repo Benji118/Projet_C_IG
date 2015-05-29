18f316dc30e14c0b7bd5b6ff561a9506695f6863
Lucas Gonon <lucas.gonon@ensimag.grenoble-inp.fr>
2015-05-29 10:15:50 +0200

    draw.c

---
+++ src/ei_draw.c
@@ -0,0 +1,38 @@
+#include "ei_draw.h"
+
+void			ei_draw_polyline	(ei_surface_t			surface,
+						 const ei_linked_point_t*	first_point,
+						 const ei_color_t		color,
+						 const ei_rect_t*		clipper) 
+{
+}
+
+void			ei_draw_polygon		(ei_surface_t			surface,
+						 const ei_linked_point_t*	first_point,
+						 const ei_color_t		color,
+						 const ei_rect_t*		clipper)
+{
+}
+
+void			ei_draw_text		(ei_surface_t		surface,
+						 const ei_point_t*	where,
+						 const char*		text,
+						 const ei_font_t	font,
+						 const ei_color_t*	color,
+						 const ei_rect_t*	clipper)
+{
+}
+
+void			ei_fill			(ei_surface_t		surface,
+						 const ei_color_t*	color,
+						 const ei_rect_t*	clipper)
+{
+}
+
+int			ei_copy_surface		(ei_surface_t		destination,
+						 const ei_rect_t*	dst_rect,
+						 const ei_surface_t	source,
+						 const ei_rect_t*	src_rect,
+						 const ei_bool_t	alpha)
+{
+}
