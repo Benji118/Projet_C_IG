f4338a410059447b77a496071b7ecdf4a246b0ba
Benji118 <benjamin.lebit@yahoo.fr>
2015-06-02 00:38:22 +0200

    avancements sur polygon

--- include/polygon.h
+++ include/polygon.h
@@ -1,6 +1,6 @@
 #ifndef POLYGON_H
 #define POLYGON_H
-
+#include <stdbool.h>
 #include "SDL_keysym.h"
 #include <stdint.h>
 #include "ei_types.h"
@@ -11,17 +11,23 @@
 */
 typedef struct ei_side_t {
   int                             ymax; // ordonnee maximale du cote
+  int 							  dx,dy,e;
   int                             x_ymin; // abscisse du point d'ordonnee minimale du cote
   float                           rec_pente; //pente reciproque du cote
   struct ei_side_t*               next; //pointeur vers le cote suivant
 } ei_side_t;
+
+typedef struct TC_t {
+	ei_side_t*** table;
+	int nbre_cote;
+}TC_t;
 
 typedef struct cel_TCA cel_TCA;
 
 struct cel_TCA {
 	ei_side_t side;
 	cel_TCA* next;
-/*	int x_inter; TODO*/
+	int x_inter;
 };
 
 typedef struct TCA {
@@ -36,4 +42,5 @@ TCA *add_TCA(TCA *a, ei_side_t c);
 //suppresion dans TCA
 TCA *del_TCA (TCA *a, ei_side_t c);
 
+TCA *sort_TCA (TCA *a);
 #endif
