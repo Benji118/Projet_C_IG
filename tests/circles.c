52aba92dadf25e79d3d8ceae99f97cda6bdeb285
Antoine Girerd <antoine.girerd@ensimag.grenoble-inp.fr>
2015-06-04 19:45:02 +0200

    fonctions pour tracer un bouton

---
+++ tests/circles.c
@@ -0,0 +1,56 @@
+#include "hw_interface.h"
+#include "ei_utils.h"
+#include "geometry.h"
+#include "ei_types.h"
+#include "ei_draw.h"
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+int ei_main(int argc, char** argv)
+{
+	ei_size_t		win_size	= ei_size(800, 600);
+	ei_surface_t		main_window	= NULL;
+	ei_color_t		color		= { 255, 0, 255, 255 };
+	ei_rect_t*		clipper_ptr	= NULL;
+//	ei_rect_t		clipper		= ei_rect(ei_point(200, 150), ei_size(400, 300));
+//	clipper_ptr		= &clipper;
+	
+	hw_init();
+		
+	main_window = hw_create_window(&win_size, EI_FALSE);
+	
+	/* Lock the drawing surface, paint it white. */
+	hw_surface_lock	(main_window);
+	//ei_fill		(main_window, &white, clipper_ptr);
+
+	/* Draw circle. */
+	ei_point_t centre = {400, 300};
+	double rayon = 150.0;
+	double dep = 270.0;
+	double arr = 360.0;
+
+	/* Test arc */
+	ei_linked_point_t *poly = malloc(sizeof(ei_linked_point_t));
+	//poly = arc(rayon, centre, dep, arr);
+	//ei_draw_polyline(main_window, poly, color, clipper_ptr);
+
+	/* Test rounded_frame */
+	ei_size_t taille = {250, 150};
+	ei_rect_t rectangle = {centre, taille};
+	poly = rounded_frame(rectangle, 20.0, false, true);
+	ei_draw_polyline(main_window, poly, color, clipper_ptr);
+	poly = rounded_frame(rectangle, 20.0, true, false);
+	ei_draw_polyline(main_window, poly, color, clipper_ptr);
+	
+	
+	/* Unlock and update the surface. */
+	hw_surface_unlock(main_window);
+	hw_surface_update_rects(main_window, NULL);
+	
+	/* Wait for a character on command line. */
+	getchar();
+	
+	hw_quit();
+	return (EXIT_SUCCESS);
+}
