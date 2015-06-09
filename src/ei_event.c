4d537ff9ae9a3b5d8f4c779064959774d82d8d74
Benji118 <benjamin.lebit@yahoo.fr>
2015-06-09 14:37:20 +0200

    initialisation toplevel pour hello_world

--- src/ei_event.c
+++ src/ei_event.c
@@ -1,6 +1,11 @@
 #include "ei_types.h"
 #include "global.h"
 #include "ei_widget.h"
+#include "ei_widgetclass.h"
+#include "ei_event.h"
+
+static ei_widget_t* handled_widget = NULL;
+static ei_default_handle_func_t handlefunc = NULL;
 
 void ei_event_set_active_widget(ei_widget_t* widget)
 {
@@ -24,9 +29,5 @@ void ei_event_set_default_handle_func(ei_default_handle_func_t func)
 
 
 ei_default_handle_func_t ei_event_get_default_handle_func()
-{
-}
-
-ei_bool_t ei_default_handle_func_t (struct ei_event_t* event)
 {
 }
